529791bc0155ccc0e0630cba6cc275c95d821502
inspirer <inspirer@e6c7545c-1420-0410-97a3-de40652d7ea1>
2007-04-12 11:12:51 +0000

    fix build, unused libraries are removed
    git-svn-id: https://lapg.svn.sourceforge.net/svnroot/lapg@25 e6c7545c-1420-0410-97a3-de40652d7ea1
    
    --HG--
    extra : convert_revision : 578c3091fa73b5a54235983fdc31a02f0df752b7

--- src/build.bat
+++ src/build.bat
@@ -11,7 +11,7 @@ cl /nologo /D "_CRT_SECURE_NO_DEPRECATE" /wd4018 txt2c++.cpp /Fe"out/" /Fo"out/"
 .\out\txt2c++.exe defaults.cpp default_cpp default_cs default_js default_java
 
 cl /nologo /O2 /Ob1 /D "WIN32" /D "NDEBUG" /D "_CONSOLE" /D "_CRT_SECURE_NO_DEPRECATE" /D "_VC80_UPGRADE=0x0600" /D "_MBCS" /GF /FD /EHsc /MT /Gy /Fo".\out/" /Fd".\out/" /W3 /TP /wd4018 /c .\templates.cpp .\srcgen.cpp .\parse.cpp .\lbuild.cpp .\lapg.cpp .\lalr1.cpp .\gbuild.cpp .\engine.cpp .\defaults.cpp .\common.cpp
-link /nologo /OUT:"out/lapg.exe" /INCREMENTAL:NO /SUBSYSTEM:CONSOLE /MACHINE:X86 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib out\common.obj out\defaults.obj out\engine.obj out\gbuild.obj out\lalr1.obj out\lapg.obj out\lbuild.obj out\parse.obj out\srcgen.obj out\templates.obj
+link /nologo /OUT:"out/lapg.exe" /INCREMENTAL:NO /SUBSYSTEM:CONSOLE /MACHINE:X86 out\common.obj out\defaults.obj out\engine.obj out\gbuild.obj out\lalr1.obj out\lapg.obj out\lbuild.obj out\parse.obj out\srcgen.obj out\templates.obj
 
 if not exist .\out\lapg.exe goto error
 
