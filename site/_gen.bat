69b392e6643abf1cdb75a184f95dede6c273d52d
inspirer <inspirer@e6c7545c-1420-0410-97a3-de40652d7ea1>
2007-04-17 11:48:08 +0000

    add download.html, update menu
    git-svn-id: https://lapg.svn.sourceforge.net/svnroot/lapg@35 e6c7545c-1420-0410-97a3-de40652d7ea1
    
    --HG--
    extra : convert_revision : d9db0627d3bf4baadf045b0fc0402bb63f838f29

--- site/_gen.bat
+++ site/_gen.bat
@@ -3,3 +3,4 @@ xsltproc --xinclude --stringparam lang eng default.xslt index.xml > public_html\
 xsltproc --xinclude --stringparam lang eng default.xslt docs.xml > public_html\docs.html
 xsltproc --xinclude --stringparam lang eng default.xslt samples.xml > public_html\samples.html
 xsltproc --xinclude --stringparam lang eng default.xslt links.xml > public_html\links.html
+xsltproc --xinclude --stringparam lang eng default.xslt download.xml > public_html\download.html
