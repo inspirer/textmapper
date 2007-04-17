7b22c80fe25674059b5e9fcacdf771f6025d29b5
inspirer <inspirer@e6c7545c-1420-0410-97a3-de40652d7ea1>
2007-04-17 10:59:37 +0000

    - update
    git-svn-id: https://lapg.svn.sourceforge.net/svnroot/lapg@34 e6c7545c-1420-0410-97a3-de40652d7ea1
    
    --HG--
    extra : convert_revision : 41a57500882dfb6cd3e111ed1fe825d33b44bad2

--- site/_gen.bat
+++ site/_gen.bat
@@ -1,2 +1,5 @@
 @echo off
 xsltproc --xinclude --stringparam lang eng default.xslt index.xml > public_html\index.html
+xsltproc --xinclude --stringparam lang eng default.xslt docs.xml > public_html\docs.html
+xsltproc --xinclude --stringparam lang eng default.xslt samples.xml > public_html\samples.html
+xsltproc --xinclude --stringparam lang eng default.xslt links.xml > public_html\links.html
