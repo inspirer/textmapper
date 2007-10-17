34ad17432c8d2215e2b445478d88c38977796dbe
inspirer <inspirer@e6c7545c-1420-0410-97a3-de40652d7ea1>
2007-10-17 07:45:34 +0000

    last arrays generated, added "${for i : 1,length}" support
    git-svn-id: https://lapg.svn.sourceforge.net/svnroot/lapg@89 e6c7545c-1420-0410-97a3-de40652d7ea1
    
    --HG--
    extra : convert_revision : ee0b3eb7913c0b5ebb31f3d782930e697f1704ea

--- java/net.sf.lapg.templates/src/net/sf/lapg/templates/ast/ForeachNode.java
+++ java/net.sf.lapg.templates/src/net/sf/lapg/templates/ast/ForeachNode.java
@@ -1,22 +1,29 @@
 package net.sf.lapg.templates.ast;
 
 import java.util.Collection;
-import java.util.List;
 
 import net.sf.lapg.templates.api.EvaluationException;
 import net.sf.lapg.templates.api.IEvaluationEnvironment;
 
 public class ForeachNode extends CompoundNode {
-	
+
 	private static final String INDEX = "index";
 	String var;
-	ExpressionNode selectExpr;
-	
+	ExpressionNode selectExpr, targetExpr;
+
 	public ForeachNode(String var, ExpressionNode selectExpr) {
 		this.var = var;
 		this.selectExpr = selectExpr;
+		this.targetExpr = null;
 	}
 
+	public ForeachNode(String var, ExpressionNode selectExpr, ExpressionNode targetExpr) {
+		this.var = var;
+		this.selectExpr = selectExpr;
+		this.targetExpr = targetExpr;
+	}
+
+	@Override
 	protected void emit(StringBuffer sb, Object context, IEvaluationEnvironment env) {
 		try {
 			Object select = env.evaluate(selectExpr, context, false);
@@ -24,8 +31,22 @@ public class ForeachNode extends CompoundNode {
 			Object prevIndex = env.getVariable(INDEX);
 			int index = 0;
 			try {
-				if( select instanceof Collection ) {
-					for( Object o : (Collection)select ) {
+				if( targetExpr != null ) {
+					Object to = env.evaluate(targetExpr, context, false);
+					if( select instanceof Integer && to instanceof Integer ) {
+						int toInt = (Integer)to;
+						int delta = toInt >= (Integer)select ? 1 : -1;
+						for( int i = (Integer)select; (delta > 0 ? i <= toInt : i >= toInt); i += delta ) {
+							env.setVariable(var, i);
+							for( Node n : instructions ) {
+								n.emit(sb, context, env);
+							}
+						}
+					} else {
+						env.fireError("In for `"+selectExpr.toString()+"` and `"+targetExpr.toString()+"` should be Integers for " + context.getClass().getCanonicalName());
+					}
+				} else if( select instanceof Collection ) {
+					for( Object o : (Collection<?>)select ) {
 						env.setVariable(var, o);
 						env.setVariable(INDEX, index++);
 						for( Node n : instructions ) {
