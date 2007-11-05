7ce9f83027b98d557a49fafd4a0ede4a71872c1a
inspirer <inspirer@e6c7545c-1420-0410-97a3-de40652d7ea1>
2007-11-05 11:40:56 +0000

    query, location of Node
    git-svn-id: https://lapg.svn.sourceforge.net/svnroot/lapg@101 e6c7545c-1420-0410-97a3-de40652d7ea1
    
    --HG--
    extra : convert_revision : b33b9561e11a4eacce5c7bc2d80bf2f5cea656e8

--- java/net.sf.lapg.templates.test/src/net/sf/lapg/templates/test/cases/XmlTest.java
+++ java/net.sf.lapg.templates.test/src/net/sf/lapg/templates/test/cases/XmlTest.java
@@ -1,20 +1,23 @@
 package net.sf.lapg.templates.test.cases;
 
+import junit.framework.Assert;
 import net.sf.lapg.templates.model.xml.XmlModel;
 import net.sf.lapg.templates.model.xml.XmlNode;
 import net.sf.lapg.templates.test.TemplateTestCase;
 
 public class XmlTest extends TemplateTestCase{
 
-	public void testForEach() {
+	public void testSelector() {
 		XmlNode n = XmlModel.load(" <r><user name='jone'/>go<user name='go'/></r> ");
-
-
 		TestEnvironment env = new TestEnvironment(getClass().getClassLoader(), "net/sf/lapg/templates/test/ltp");
 
 		// test 1
-//		String q = env.executeTemplate("xml.show1", new HashMap<Object,Object>(), null);
-//		Assert.assertEquals("???", q);
+		String q = env.executeTemplate("loop.xmldo", n, null);
+		Assert.assertEquals("jone\r\ngo\r\n", q);
+		env.assertEmptyErrors();
+
+		q = env.executeTemplate("loop.selectit", n, null);
+		Assert.assertEquals("jone\r\n", q);
 		env.assertEmptyErrors();
 	}
 }
