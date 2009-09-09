5c3e0f15022fd73b9f3789e48696a4b85a040bfa
inspirer <inspirer@e6c7545c-1420-0410-97a3-de40652d7ea1>
2009-09-09 17:48:09 +0000

    lexer tests, including unicode
    git-svn-id: https://lapg.svn.sourceforge.net/svnroot/lapg@233 e6c7545c-1420-0410-97a3-de40652d7ea1
    
    --HG--
    extra : convert_revision : c5451f79287a9c2ec585e46fc077a9c563965aab

--- java/net.sf.lapg.test/src/net/sf/lapg/test/TestRunner.java
+++ java/net.sf.lapg.test/src/net/sf/lapg/test/TestRunner.java
@@ -6,12 +6,14 @@ import net.sf.lapg.test.cases.CharacterSetTest;
 import net.sf.lapg.test.cases.ConsoleArgsTest;
 import net.sf.lapg.test.cases.InputTest;
 import net.sf.lapg.test.cases.JavaTablesCompression;
+import net.sf.lapg.test.cases.LexerGeneratorTest;
 
 
 public class TestRunner {
     public static Test suite() {
         TestSuite ts = new TestSuite("lapg tests");
         ts.addTestSuite(CharacterSetTest.class);
+        ts.addTestSuite(LexerGeneratorTest.class);
         ts.addTestSuite(InputTest.class);
         ts.addTestSuite(JavaTablesCompression.class);
         ts.addTestSuite(ConsoleArgsTest.class);
---
+++ java/net.sf.lapg.test/src/net/sf/lapg/test/cases/LexerGeneratorTest.java
@@ -0,0 +1,110 @@
+package net.sf.lapg.test.cases;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+
+import net.sf.lapg.LexerTables;
+import net.sf.lapg.api.Action;
+import net.sf.lapg.api.Lexem;
+import net.sf.lapg.api.Symbol;
+import net.sf.lapg.lex.LexicalBuilder;
+import net.sf.lapg.test.TestNotifier;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class LexerGeneratorTest extends TestCase {
+
+	TestLexem[] INPUT1 = {
+		new TestLexem(0, 0, "string", "[a-z][A-Z]?", "bC", "aZ", "zA", "q"),
+		new TestLexem(1, 0, "number", "[0-9]+", "1", "12", "323", "2111111"),
+		new TestLexem(2, 0, "hex", "0x[0-9a-zA-Z]+", "0x23bd", "0x1", "0x0"),
+		new TestLexem(3, 0, "hex", "\\n|\\t|\\r", "\n", "\t", "\r"),
+		new TestLexem(4, 0, "hex", "[\\xAAAA-\\xAABB]", "\uaab0", "\uaabb", "\uaaaa", "\uaaaf"),
+	};
+	
+	public void testGenerator() {
+		LexerTables lt = LexicalBuilder.compile(INPUT1, new TestNotifier(), 0);
+		for(TestLexem tl : INPUT1) {
+			for(String s : tl.getSamples()) {
+				int res = nextToken(lt, s);
+				Assert.assertEquals("For "+s+" Expected " + tl.getRegexp()+ ";",tl.index, res);
+			}			
+		}		
+	}
+	
+	private int nextToken(LexerTables lr, String s) {
+		int state = 0;
+		int index = 0;
+
+		while(state >= 0) {
+			int chr = index < s.length() ? s.codePointAt(index++) : 0;
+			state = lr.change[state][chr>=0 && chr < lr.char2no.length ? lr.char2no[chr]: 1];
+		}
+		if( state == -1 ) {
+			return -1;
+		}
+		return -state-2;
+	}
+
+	private static class TestLexem implements Lexem {
+
+		private int index;
+		private int prio;
+		private String name;
+		private String regexp;
+		private String[] samples;
+		
+		public TestLexem(int index, int prio, String name, String regexp, String ... samples) {
+			this.index = index;
+			this.prio = prio;
+			this.name = name;
+			this.regexp = regexp;
+			this.samples = samples;
+		}
+
+		public Action getAction() {
+			return null;
+		}
+
+		public int getGroups() {
+			return 1;
+		}
+
+		public int getPriority() {
+			return prio;
+		}
+
+		public String getRegexp() {
+			return regexp;
+		}
+		
+		public String[] getSamples() {
+			return samples;
+		}
+
+		public Symbol getSymbol() {
+			return new Symbol() {
+				public boolean isTerm() {
+					throw new NotImplementedException();
+				}
+				
+				public boolean isDefined() {
+					throw new NotImplementedException();
+				}
+				
+				public String getType() {
+					throw new NotImplementedException();
+				}
+				
+				public String getName() {
+					return name;
+				}
+				
+				public int getIndex() {
+					return index;
+				}
+			};
+		}
+	}
+	
+}
