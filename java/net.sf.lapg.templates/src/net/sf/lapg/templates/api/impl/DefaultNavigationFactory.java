e632b1bafc02b51f18595c4db9d997c3a4d93db9
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2010-03-14 19:09:55 +0100

    support array.contains, started isList
    --HG--
    extra : convert_revision : 32c196510971081c636e429af322748d74131634

--- java/net.sf.lapg.templates/src/net/sf/lapg/templates/api/impl/DefaultNavigationFactory.java
+++ java/net.sf.lapg.templates/src/net/sf/lapg/templates/api/impl/DefaultNavigationFactory.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 
@@ -21,23 +22,23 @@ public class DefaultNavigationFactory implements INavigationStrategy.Factory {
 
 	public INavigationStrategy<?> getStrategy(Object o) {
 
-		if( o instanceof Object[] ) {
+		if (o instanceof Object[]) {
 			return arrayNavigation;
 		}
 
-		if( o instanceof int[] ) {
+		if (o instanceof int[]) {
 			return intArrayNavigation;
 		}
 
-		if( o instanceof Map<?, ?> ) {
+		if (o instanceof Map<?, ?>) {
 			return mapNavigation;
 		}
 
-		if( o instanceof String ) {
-			//return stringNavigation;
+		if (o instanceof String) {
+			// return stringNavigation;
 		}
 
-		if( o instanceof Collection<?> ) {
+		if (o instanceof Collection<?>) {
 			return collectionNavigation;
 		}
 
@@ -67,47 +68,47 @@ public class DefaultNavigationFactory implements INavigationStrategy.Factory {
 		}
 
 		@SuppressWarnings("unchecked")
-		public Object callMethod( Object obj, String methodName, Object[] args ) throws EvaluationException {
+		public Object callMethod(Object obj, String methodName, Object[] args) throws EvaluationException {
 			try {
 				Class[] argClasses = null;
-				if( args != null ) {
+				if (args != null) {
 					argClasses = new Class[args.length];
-					for( int i = 0; i < args.length; i++ ) {
+					for (int i = 0; i < args.length; i++) {
 						argClasses[i] = args[i].getClass();
 					}
 				}
 				Method meth = null;
 				try {
 					meth = obj.getClass().getMethod(methodName, argClasses);
-				} catch(NoSuchMethodException ex) {
+				} catch (NoSuchMethodException ex) {
 					meth = searchMethod(obj.getClass(), methodName, argClasses);
-					if(meth == null) {
-						throw new EvaluationException("no method: " + ex.toString() );
+					if (meth == null) {
+						throw new EvaluationException("no method: " + ex.toString());
 					}
 				}
 				return meth.invoke(obj, args);
-			} catch( IllegalAccessException ex ) {
+			} catch (IllegalAccessException ex) {
 				throw new EvaluationException("IllegalAccessException");
-			} catch( InvocationTargetException ex ) {
+			} catch (InvocationTargetException ex) {
 				throw new EvaluationException("InvocationTargetException");
 			}
 		}
 
 		private Method searchMethod(Class<?> class1, String methodName, Class<?>[] argClasses) {
-			for(Method m : class1.getMethods()) {
-				if(m.getName().equals(methodName)) {
+			for (Method m : class1.getMethods()) {
+				if (m.getName().equals(methodName)) {
 					Class<?>[] paramTypes = m.getParameterTypes();
-					if(paramTypes.length != argClasses.length) {
+					if (paramTypes.length != argClasses.length) {
 						continue;
 					}
 					boolean good = true;
-					for(int i = 0; i < paramTypes.length; i++) {
-						if(!paramTypes[i].isAssignableFrom(argClasses[i])) {
+					for (int i = 0; i < paramTypes.length; i++) {
+						if (!paramTypes[i].isAssignableFrom(argClasses[i])) {
 							good = false;
 							break;
 						}
 					}
-					if(good) {
+					if (good) {
 						return m;
 					}
 				}
@@ -120,17 +121,17 @@ public class DefaultNavigationFactory implements INavigationStrategy.Factory {
 		}
 	};
 
-	private final INavigationStrategy<Map<?,?>> mapNavigation = new INavigationStrategy<Map<?,?>>() {
+	private final INavigationStrategy<Map<?, ?>> mapNavigation = new INavigationStrategy<Map<?, ?>>() {
 
-		public Object callMethod(Map<?,?> obj, String methodName, Object[] args) throws EvaluationException {
+		public Object callMethod(Map<?, ?> obj, String methodName, Object[] args) throws EvaluationException {
 			return javaNavigation.callMethod(obj, methodName, args);
 		}
 
-		public Object getByIndex(Map<?,?> obj, Object index) throws EvaluationException {
+		public Object getByIndex(Map<?, ?> obj, Object index) throws EvaluationException {
 			return obj.get(index);
 		}
 
-		public Object getProperty(Map<?,?> obj, String id) throws EvaluationException {
+		public Object getProperty(Map<?, ?> obj, String id) throws EvaluationException {
 			return obj.get(id);
 		}
 	};
@@ -138,8 +139,8 @@ public class DefaultNavigationFactory implements INavigationStrategy.Factory {
 	private final INavigationStrategy<Collection<?>> collectionNavigation = new INavigationStrategy<Collection<?>>() {
 
 		public Object callMethod(Collection<?> cl, String methodName, Object[] args) throws EvaluationException {
-			if( args == null ) {
-				if( methodName.equals("first") ) {
+			if (args == null) {
+				if (methodName.equals("first")) {
 					return cl.isEmpty() ? null : cl.iterator().next();
 				}
 			}
@@ -147,42 +148,47 @@ public class DefaultNavigationFactory implements INavigationStrategy.Factory {
 		}
 
 		public Object getByIndex(Collection<?> cl, Object index) throws EvaluationException {
-			ArrayList<?> array = (ArrayList<?>)cl; // FIXME support collection by index
-			if( index instanceof Integer ) {
-				return array.get((Integer)index);
+			ArrayList<?> array = (ArrayList<?>) cl; // FIXME support collection
+													// by index
+			if (index instanceof Integer) {
+				return array.get((Integer) index);
 			} else {
 				throw new EvaluationException("index object should be integer");
 			}
 		}
 
 		public Object getProperty(Collection<?> cl, String id) throws EvaluationException {
-			if( id.equals("length") ) {
+			if (id.equals("length")) {
 				return cl.size();
 			}
-			throw new EvaluationException("do not know property `"+id+"`");
+			throw new EvaluationException("do not know property `" + id + "`");
 		}
 	};
 
 	private final INavigationStrategy<Object[]> arrayNavigation = new INavigationStrategy<Object[]>() {
 
 		public Object callMethod(Object[] array, String methodName, Object[] args) throws EvaluationException {
-			if( args == null ) {
-				if( methodName.equals("first") ) {
+			if (args == null) {
+				if (methodName.equals("first")) {
 					return array[0];
-				} else if( methodName.equals("last") ) {
-					return array[array.length-1];
-				} else if( methodName.equals("size") ) {
+				} else if (methodName.equals("last")) {
+					return array[array.length - 1];
+				} else if (methodName.equals("size")) {
 					return array.length;
 				}
+			} else if(args.length == 1) {
+				if (methodName.equals("contains")) {
+					return Arrays.asList(array).contains(args[0]);
+				}
 			}
-			throw new EvaluationException("do not know method `"+methodName+"`");
+			throw new EvaluationException("do not know method `" + methodName + "`");
 		}
 
 		public Object getByIndex(Object[] array, Object index) throws EvaluationException {
-			if( index instanceof Integer ) {
-				int i = (Integer)index;
-				if( i < 0 || i >= array.length ) {
-					throw new EvaluationException(i + " is out of 0.." + (array.length-1));
+			if (index instanceof Integer) {
+				int i = (Integer) index;
+				if (i < 0 || i >= array.length) {
+					throw new EvaluationException(i + " is out of 0.." + (array.length - 1));
 				}
 				return array[i];
 			} else {
@@ -191,33 +197,33 @@ public class DefaultNavigationFactory implements INavigationStrategy.Factory {
 		}
 
 		public Object getProperty(Object[] array, String id) throws EvaluationException {
-			if( id.equals("length") ) {
+			if (id.equals("length")) {
 				return array.length;
 			}
-			throw new EvaluationException("do not know property `"+id+"`");
+			throw new EvaluationException("do not know property `" + id + "`");
 		}
 	};
 
 	private final INavigationStrategy<int[]> intArrayNavigation = new INavigationStrategy<int[]>() {
 
 		public Object callMethod(int[] array, String methodName, Object[] args) throws EvaluationException {
-			if( args == null ) {
-				if( methodName.equals("first") ) {
+			if (args == null) {
+				if (methodName.equals("first")) {
 					return array[0];
-				} else if( methodName.equals("last") ) {
-					return array[array.length-1];
-				} else if( methodName.equals("size") ) {
+				} else if (methodName.equals("last")) {
+					return array[array.length - 1];
+				} else if (methodName.equals("size")) {
 					return array.length;
 				}
 			}
-			throw new EvaluationException("do not know method `"+methodName+"`");
+			throw new EvaluationException("do not know method `" + methodName + "`");
 		}
 
 		public Object getByIndex(int[] array, Object index) throws EvaluationException {
-			if( index instanceof Integer ) {
-				int i = (Integer)index;
-				if( i < 0 || i >= array.length ) {
-					throw new EvaluationException(i + " is out of 0.." + (array.length-1));
+			if (index instanceof Integer) {
+				int i = (Integer) index;
+				if (i < 0 || i >= array.length) {
+					throw new EvaluationException(i + " is out of 0.." + (array.length - 1));
 				}
 				return array[i];
 			} else {
@@ -226,10 +232,10 @@ public class DefaultNavigationFactory implements INavigationStrategy.Factory {
 		}
 
 		public Object getProperty(int[] array, String id) throws EvaluationException {
-			if( id.equals("length") ) {
+			if (id.equals("length")) {
 				return array.length;
 			}
-			throw new EvaluationException("do not know property `"+id+"`");
+			throw new EvaluationException("do not know property `" + id + "`");
 		}
 	};
 }
