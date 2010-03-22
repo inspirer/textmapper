1805e64ae8c6db32e0f45e794218ee9b268e4e6f
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2010-03-22 23:44:02 +0100

    better interfaceRules()
    --HG--
    extra : convert_revision : c962ccd68389a936cd6c34ec4c353916c4fbc865

--- java/net.sf.lapg/src/net/sf/lapg/gen/options/ast/IInput.java
+++ java/net.sf.lapg/src/net/sf/lapg/gen/options/ast/IInput.java
@@ -3,5 +3,7 @@ package net.sf.lapg.gen.options.ast;
 public interface IInput {
 
 // input ::= groups (normal)
+// modifiersopt ::= modifiers (normal)
+// optionslistopt ::= optionslist (normal)
 	
 }
