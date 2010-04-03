04976a7c5efbcef3425d4919fc80753bb6d35a94
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2010-04-03 14:28:38 +0200

    lexer.reset, remove obsolete exception declaration, fix template
    --HG--
    extra : convert_revision : cc52658d1fd9f05949c2543459ec6f18820f52aa

--- java/net.sf.lapg/src/net/sf/lapg/parser/LapgLexer.java
+++ java/net.sf.lapg/src/net/sf/lapg/parser/LapgLexer.java
@@ -2,7 +2,6 @@ package net.sf.lapg.parser;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 import java.text.MessageFormat;
 
 public class LapgLexer {
@@ -54,14 +53,14 @@ public class LapgLexer {
 
 	public static final int TOKEN_SIZE = 2048;
 
-	final private Reader stream;
+	private Reader stream;
 	final private ErrorReporter reporter;
 
 	final private char[] data = new char[2048];
 	private int datalen, l;
 	private char chr;
 
-	private int group = 0;
+	private int group;
 
 	final private StringBuilder token = new StringBuilder(TOKEN_SIZE);
 
@@ -110,10 +109,15 @@ public class LapgLexer {
 	}
 
 	public LapgLexer(Reader stream, ErrorReporter reporter) throws IOException {
-		this.stream = stream;
 		this.reporter = reporter;
+		reset(stream);
+	}
+
+	public void reset(Reader stream) throws IOException {
+		this.stream = stream;
 		this.datalen = stream.read(data);
 		this.l = 0;
+		this.group = 0;
 		chr = l < datalen ? data[l++] : 0;
 	}
 
@@ -203,7 +207,7 @@ public class LapgLexer {
 		return 1;
 	}
 
-	public LapgSymbol next() throws IOException, UnsupportedEncodingException {
+	public LapgSymbol next() throws IOException {
 		LapgSymbol lapg_n = new LapgSymbol();
 		int state;
 
@@ -255,7 +259,7 @@ public class LapgLexer {
 		return lapg_n;
 	}
 
-	private boolean createToken(LapgSymbol lapg_n) throws UnsupportedEncodingException {
+	protected boolean createToken(LapgSymbol lapg_n) {
 		switch( lapg_n.lexem ) {
 			case 1:
 				 lapg_n.sym = current(); break; 
