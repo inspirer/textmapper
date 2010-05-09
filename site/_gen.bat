32f70651b1fef3686a32b3e677e3b2f28cbda820
egryaznov <none@none>
2010-05-09 23:56:36 +0200

    remove old

--- site/_gen.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-xsltproc --xinclude --stringparam lang eng default.xslt index.xml > public_html\index.html
-xsltproc --xinclude --stringparam lang eng default.xslt docs.xml > public_html\docs.html
-xsltproc --xinclude --stringparam lang eng default.xslt samples.xml > public_html\samples.html
-xsltproc --xinclude --stringparam lang eng default.xslt links.xml > public_html\links.html
-xsltproc --xinclude --stringparam lang eng default.xslt download.xml > public_html\download.html
