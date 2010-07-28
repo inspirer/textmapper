d874cd32661144f2c3ae8ae25b634009b815030a
Evgeny Gryaznov <egryaznov@gmail.com>
2010-07-29 01:02:17 +0200

    test new array packer: int[]

--- java/net.sf.lapg.test/src/net/sf/lapg/test/cases/JavaTablesCompression.java
+++ java/net.sf.lapg.test/src/net/sf/lapg/test/cases/JavaTablesCompression.java
@@ -1,6 +1,6 @@
 /**
  * Copyright 2002-2010 Evgeny Gryaznov
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -48,6 +48,31 @@ public class JavaTablesCompression extends TestCase {
 				if( a[i][e] != b[i][e] ) {
 					Assert.fail("wrong decompression at "+i+","+e);
 				}
+			}
+		}
+	}
+
+	private void checkDecompression(int[] a) {
+		String c = JavaArrayArchiver.packInt(a, 5);
+
+		StringBuffer extractedString = new StringBuffer();
+		char[] chs = c.toCharArray();
+		boolean isstring = false;
+		for (char ch : chs) {
+			if( ch == '"') {
+				isstring = !isstring;
+				continue;
+			}
+			if( isstring ) {
+				extractedString.append(ch);
+			}
+		}
+		Assert.assertFalse(isstring);
+
+		int[] b = JavaArrayArchiver.unpackInt(a.length, extractedString.toString());
+		for( int i = 0; i < a.length; i++) {
+			if( a[i] != b[i] ) {
+				Assert.fail("wrong decompression at "+i);
 			}
 		}
 	}
@@ -115,5 +140,36 @@ public class JavaTablesCompression extends TestCase {
 				{1,2,3,4,5,6,7,8,4,4,4,4,4,4,4,4,5,6,2,1,1,1,1,1,1,43,345,345,345,34,34,345,34,345,34533,333},
 				{1,2,3,4,5,6,7,8,4,4,4,4,4,4,4,4,5,6,2,1,1,1,1,1,1,43,345,345,345,34,34,345,34,345,34533,333},
 		});
+	}
+
+	public void testIntCompression1() {
+		checkDecompression(new int[] {1,2,3,4,5,6,7,8});
+	}
+
+	public void testIntCompressionEmpty() {
+		checkDecompression(new int[] {});
+	}
+
+	public void testIntCompressionOne() {
+		checkDecompression(new int[] { -100 });
+		checkDecompression(new int[] { 1 });
+	}
+
+	public void testIntCompressionNearZero() {
+		checkDecompression(new int[] { 0 });
+		checkDecompression(new int[] { -1 });
+	}
+
+	public void testIntCompression2() {
+		checkDecompression(new int[] { 0, 0, 0 });
+		checkDecompression(new int[] { Integer.MAX_VALUE });
+	}
+
+	public void testIntCompressionMax() {
+		checkDecompression(new int[] { Integer.MAX_VALUE });
+	}
+
+	public void testIntCompressionMin() {
+		checkDecompression(new int[] { Integer.MIN_VALUE });
 	}
 }
