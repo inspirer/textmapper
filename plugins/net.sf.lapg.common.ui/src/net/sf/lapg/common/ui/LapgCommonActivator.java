3db6c14c4ba42cb495ae8b553e7b43915f3beae9
Evgeny Gryaznov <egryaznov@gmail.com>
2010-10-06 20:14:35 +0200

    add log() operations

--- plugins/net.sf.lapg.common.ui/src/net/sf/lapg/common/ui/LapgCommonActivator.java
+++ plugins/net.sf.lapg.common.ui/src/net/sf/lapg/common/ui/LapgCommonActivator.java
@@ -1,6 +1,6 @@
 /**
  * This file is part of Lapg.UI project.
- * 
+ *
  * Copyright (c) 2010 Evgeny Gryaznov
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,6 +12,8 @@
  */
 package net.sf.lapg.common.ui;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -22,6 +24,14 @@ public class LapgCommonActivator extends AbstractUIPlugin {
 	private static LapgCommonActivator plugin;
 
 	public LapgCommonActivator() {
+	}
+
+	public static void log(Throwable e) {
+		log(new Status(IStatus.ERROR, PLUGIN_ID, 0, "Internal error", e));
+	}
+
+	public static void log(IStatus status) {
+		getDefault().getLog().log(status);
 	}
 
 	@Override
