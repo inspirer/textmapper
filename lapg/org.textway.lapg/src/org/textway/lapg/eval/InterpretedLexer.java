807f81bc22ec044c896941f4965b61924653bbc9
Evgeny Gryaznov <egryaznov@gmail.com>
2011-03-19 00:04:34 +0100

    interpreted parser (initial commit)

---
+++ lapg/org.textway.lapg/src/org/textway/lapg/eval/InterpretedLexer.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright 2002-2010 Evgeny Gryaznov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.textway.lapg.eval;
+
+import org.textway.lapg.lex.LexerTables;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.text.MessageFormat;
+
+/**
+ * Gryaznov Evgeny, 3/17/11
+ */
+public class InterpretedLexer {
+
+	public static class InterpretedSymbol {
+		public Object sym;
+		public int lexem;
+		public int state;
+		public int line;
+		public int offset;
+		public int endoffset;
+	}
+
+	public interface ErrorReporter {
+		void error(int start, int end, int line, String s);
+	}
+
+	public static final int TOKEN_SIZE = 2048;
+
+	private Reader stream;
+	final private ErrorReporter reporter;
+
+	final private char[] data = new char[2048];
+	private int datalen, l;
+	private char chr;
+
+	private int group;
+
+	final private StringBuilder token = new StringBuilder(TOKEN_SIZE);
+
+	private int tokenLine = 1;
+	private int currLine = 1;
+	private int currOffset = 0;
+
+	private final int[] lapg_char2no;
+	private final int[] lapg_lexemnum;
+	private final int[][] lapg_lexem;
+
+
+	public InterpretedLexer(Reader stream, ErrorReporter reporter, LexerTables tables) throws IOException {
+		this.reporter = reporter;
+		this.lapg_char2no = tables.char2no;
+		this.lapg_lexem = tables.change;
+		this.lapg_lexemnum = tables.lnum;
+
+		reset(stream);
+	}
+
+	public void reset(Reader stream) throws IOException {
+		this.stream = stream;
+		this.datalen = stream.read(data);
+		this.l = 0;
+		this.group = 0;
+		chr = l < datalen ? data[l++] : 0;
+	}
+
+	public int getState() {
+		return group;
+	}
+
+	public void setState(int state) {
+		this.group = state;
+	}
+
+	public int getTokenLine() {
+		return tokenLine;
+	}
+
+	public void setLine(int currLine) {
+		this.currLine = currLine;
+	}
+
+	public void setOffset(int currOffset) {
+		this.currOffset = currOffset;
+	}
+
+	public String current() {
+		return token.toString();
+	}
+
+	private int mapCharacter(int chr) {
+		if (chr >= 0 && chr < 128) {
+			return lapg_char2no[chr];
+		}
+		return 1;
+	}
+
+	public InterpretedSymbol next() throws IOException {
+		InterpretedSymbol lapg_n = new InterpretedSymbol();
+		int state;
+
+		do {
+			lapg_n.offset = currOffset;
+			tokenLine = lapg_n.line = currLine;
+			if (token.length() > TOKEN_SIZE) {
+				token.setLength(TOKEN_SIZE);
+				token.trimToSize();
+			}
+			token.setLength(0);
+			int tokenStart = l - 1;
+
+			for (state = group; state >= 0;) {
+				state = lapg_lexem[state][mapCharacter(chr)];
+				if (state >= -1 && chr != 0) {
+					currOffset++;
+					if (chr == '\n') {
+						currLine++;
+					}
+					if (l >= datalen) {
+						token.append(data, tokenStart, l - tokenStart);
+						datalen = stream.read(data);
+						tokenStart = l = 0;
+					}
+					chr = l < datalen ? data[l++] : 0;
+				}
+			}
+			lapg_n.endoffset = currOffset;
+
+			if (state == -1) {
+				if (chr == 0) {
+					reporter.error(lapg_n.offset, lapg_n.endoffset, currLine, "Unexpected end of file reached");
+					break;
+				}
+				reporter.error(lapg_n.offset, lapg_n.endoffset, currLine, MessageFormat.format("invalid lexem at line {0}: `{1}`, skipped", currLine, current()));
+				lapg_n.lexem = -1;
+				continue;
+			}
+
+			if (state == -2) {
+				lapg_n.lexem = 0;
+				lapg_n.sym = null;
+				return lapg_n;
+			}
+
+			if (l - 1 > tokenStart) {
+				token.append(data, tokenStart, l - 1 - tokenStart);
+			}
+
+			lapg_n.lexem = lapg_lexemnum[-state - 3];
+			lapg_n.sym = null;
+
+		} while (lapg_n.lexem == -1 || !createToken(lapg_n, -state - 3));
+		return lapg_n;
+	}
+
+	protected boolean createToken(InterpretedSymbol lapg_n, int lexemIndex) {
+		return true;
+	}
+}
---
+++ lapg/org.textway.lapg/src/org/textway/lapg/eval/InterpretedParser.java
@@ -0,0 +1,202 @@
+package org.textway.lapg.eval;
+
+import org.textway.lapg.api.Grammar;
+import org.textway.lapg.eval.InterpretedLexer.ErrorReporter;
+import org.textway.lapg.eval.InterpretedLexer.InterpretedSymbol;
+import org.textway.lapg.eval.InterpretedTree.TextSource;
+import org.textway.lapg.lalr.ParserTables;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+
+/**
+ * Gryaznov Evgeny, 3/17/11
+ */
+public class InterpretedParser {
+	public static class ParseException extends Exception {
+		private static final long serialVersionUID = 1L;
+
+		public ParseException() {
+		}
+	}
+
+	private final ErrorReporter reporter;
+
+	private final Grammar grammar;
+	private final int[] lapg_action;
+	private final short[] lapg_lalr;
+	private final short[] lapg_sym_goto;
+	private final short[] lapg_sym_from;
+	private final short[] lapg_sym_to;
+	private final int[] lapg_rlen;
+	private final int[] lapg_rlex;
+
+	private final boolean debugSyntax;
+
+
+	public InterpretedParser(ErrorReporter reporter, ParserTables tables, Grammar grammar, boolean debugSyntax) {
+		this.reporter = reporter;
+		this.grammar = grammar;
+		this.lapg_action = tables.action_index;
+		this.lapg_lalr = tables.action_table;
+		this.lapg_sym_goto = tables.sym_goto;
+		this.lapg_sym_to = tables.sym_to;
+		this.lapg_sym_from = tables.sym_from;
+		this.lapg_rlen = tables.getRuleLength();
+		this.lapg_rlex = tables.rleft;
+		this.debugSyntax = debugSyntax;
+	}
+
+	protected final int lapg_next(int state, int symbol) {
+		int p;
+		if (lapg_action[state] < -2) {
+			for (p = -lapg_action[state] - 3; lapg_lalr[p] >= 0; p += 2) {
+				if (lapg_lalr[p] == symbol) {
+					break;
+				}
+			}
+			return lapg_lalr[p + 1];
+		}
+		return lapg_action[state];
+	}
+
+	protected final int lapg_state_sym(int state, int symbol) {
+		int min = lapg_sym_goto[symbol], max = lapg_sym_goto[symbol + 1] - 1;
+		int i, e;
+
+		while (min <= max) {
+			e = (min + max) >> 1;
+			i = lapg_sym_from[e];
+			if (i == state) {
+				return lapg_sym_to[e];
+			} else if (i < state) {
+				min = e + 1;
+			} else {
+				max = e - 1;
+			}
+		}
+		return -1;
+	}
+
+	protected int lapg_head;
+	protected InterpretedSymbol[] lapg_m;
+	protected InterpretedSymbol lapg_n;
+
+	public Object parse(InterpretedLexer lexer, int initialState) throws IOException, ParseException {
+
+		lapg_m = new InterpretedSymbol[1024];
+		lapg_head = 0;
+		int lapg_symbols_ok = 4;
+
+		lapg_m[0] = new InterpretedSymbol();
+		lapg_m[0].state = initialState;
+		lapg_n = lexer.next();
+
+		while (lapg_m[lapg_head].state != 193+initialState) {
+			int lapg_i = lapg_next(lapg_m[lapg_head].state, lapg_n.lexem);
+
+			if (lapg_i >= 0) {
+				reduce(lapg_i);
+			} else if (lapg_i == -1) {
+				shift(lexer);
+				lapg_symbols_ok++;
+			}
+
+			if (lapg_i == -2 || lapg_m[lapg_head].state == -1) {
+				if (restore()) {
+					if (lapg_symbols_ok >= 4) {
+						reporter.error(lapg_n.offset, lapg_n.endoffset, lexer.getTokenLine(), MessageFormat.format("syntax error before line {0}", lexer.getTokenLine()));
+					}
+					if (lapg_symbols_ok <= 1) {
+						lapg_n = lexer.next();
+					}
+					lapg_symbols_ok = 0;
+					continue;
+				}
+				if (lapg_head < 0) {
+					lapg_head = 0;
+					lapg_m[0] = new InterpretedSymbol();
+					lapg_m[0].state = initialState;
+				}
+				break;
+			}
+		}
+
+		if (lapg_m[lapg_head].state != 193+initialState) {
+			if (lapg_symbols_ok >= 4) {
+				reporter.error(lapg_n.offset, lapg_n.endoffset, lexer.getTokenLine(), MessageFormat.format("syntax error before line {0}", lexer.getTokenLine()));
+			}
+			throw new ParseException();
+		}
+		return lapg_m[lapg_head - 1].sym;
+	}
+
+	protected boolean restore() {
+		if (lapg_n.lexem == 0) {
+			return false;
+		}
+		while (lapg_head >= 0 && lapg_state_sym(lapg_m[lapg_head].state, 1) == -1) {
+			dispose(lapg_m[lapg_head]);
+			lapg_m[lapg_head] = null;
+			lapg_head--;
+		}
+		if (lapg_head >= 0) {
+			lapg_m[++lapg_head] = new InterpretedSymbol();
+			lapg_m[lapg_head].lexem = grammar.getError().getIndex();
+			lapg_m[lapg_head].sym = null;
+			lapg_m[lapg_head].state = lapg_state_sym(lapg_m[lapg_head - 1].state, 1);
+			lapg_m[lapg_head].line = lapg_n.line;
+			lapg_m[lapg_head].offset = lapg_n.offset;
+			lapg_m[lapg_head].endoffset = lapg_n.endoffset;
+			return true;
+		}
+		return false;
+	}
+
+	protected void shift(InterpretedLexer lexer) throws IOException {
+		lapg_m[++lapg_head] = lapg_n;
+		lapg_m[lapg_head].state = lapg_state_sym(lapg_m[lapg_head - 1].state, lapg_n.lexem);
+		if (debugSyntax) {
+			System.out.println(MessageFormat.format("shift: {0} ({1})", grammar.getSymbols()[lapg_n.lexem].getName(), lexer.current()));
+		}
+		if (lapg_m[lapg_head].state != -1 && lapg_n.lexem != 0) {
+			lapg_n = lexer.next();
+		}
+	}
+
+	protected void reduce(int rule) {
+		InterpretedSymbol lapg_gg = new InterpretedSymbol();
+		lapg_gg.sym = (lapg_rlen[rule] != 0) ? lapg_m[lapg_head + 1 - lapg_rlen[rule]].sym : null;
+		lapg_gg.lexem = lapg_rlex[rule];
+		lapg_gg.state = 0;
+		if (debugSyntax) {
+			System.out.println("reduce to " + grammar.getSymbols()[lapg_rlex[rule]].getName());
+		}
+		InterpretedSymbol startsym = (lapg_rlen[rule] != 0) ? lapg_m[lapg_head + 1 - lapg_rlen[rule]] : lapg_n;
+		lapg_gg.line = startsym.line;
+		lapg_gg.offset = startsym.offset;
+		lapg_gg.endoffset = (lapg_rlen[rule] != 0) ? lapg_m[lapg_head].endoffset : lapg_n.offset;
+		applyRule(lapg_gg, rule, lapg_rlen[rule]);
+		for (int e = lapg_rlen[rule]; e > 0; e--) {
+			cleanup(lapg_m[lapg_head]);
+			lapg_m[lapg_head--] = null;
+		}
+		lapg_m[++lapg_head] = lapg_gg;
+		lapg_m[lapg_head].state = lapg_state_sym(lapg_m[lapg_head-1].state, lapg_gg.lexem);
+	}
+
+	protected void applyRule(InterpretedSymbol lapg_gg, int rule, int ruleLength) {
+	}
+
+	/**
+	 *  disposes symbol dropped by error recovery mechanism
+	 */
+	protected void dispose(InterpretedSymbol sym) {
+	}
+
+	/**
+	 *  cleans node removed from the stack
+	 */
+	protected void cleanup(InterpretedSymbol sym) {
+	}
+}
