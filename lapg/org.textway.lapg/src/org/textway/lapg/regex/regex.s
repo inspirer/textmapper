2f8a801c0f2b62212ae94cd76b2f7f7b10a8cc0b
Evgeny Gryaznov <egryaznov@gmail.com>
2011-05-18 23:12:19 +0200

    copyright for generated code

--- lapg/org.textway.lapg/src/org/textway/lapg/regex/regex.s
+++ lapg/org.textway.lapg/src/org/textway/lapg/regex/regex.s
@@ -1,18 +1,18 @@
-#  syntax: regular expression grammar
+# syntax: regular expression grammar
 
-#  Copyright 2002-2011 Evgeny Gryaznov
+# Copyright 2002-2011 Evgeny Gryaznov
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 lang = "java"
 prefix = "RegexDef"
@@ -21,6 +21,7 @@ breaks = true
 gentree = true
 positions = "offset"
 endpositions = "offset"
+genCopyright = true
 
 char(Character): /[^(){}\[\]\.\|\\\/*?+^-]/      							{ $lexem = current().charAt(0); break; }
 char(Character): /\\[^\r\n\t0-9uUxXwWsSdD]/									{ $lexem = RegexUtil.unescape(current().charAt(1)); break; }
