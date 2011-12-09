344f55f3b580a54e1fb8e5cd7f8a71361b397d2a
Evgeny Gryaznov <egryaznov@gmail.com>
2011-12-09 12:14:35 +0700

    gradle: publish artifacts into local libs folder; format source

--- lapg/org.textway.lapg/src/org/textway/lapg/api/Symbol.java
+++ lapg/org.textway.lapg/src/org/textway/lapg/api/Symbol.java
@@ -18,7 +18,7 @@ package org.textway.lapg.api;
 public interface Symbol extends Annotated, SourceElement {
 
 	int getIndex();
-    String getId();
+	String getId();
 	String getName();
 	String getType();
 	boolean isTerm();
