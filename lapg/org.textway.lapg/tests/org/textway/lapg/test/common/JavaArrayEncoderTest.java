e492d9c95a1e5386c49a0862037f56479eca010a
Evgeny Gryaznov <egryaznov@gmail.com>
2012-02-26 13:22:59 +0100

    move o.t.lapg.common tests into separate package

--- lapg/org.textway.lapg/tests/org/textway/lapg/test/common/JavaArrayEncoderTest.java
+++ lapg/org.textway.lapg/tests/org/textway/lapg/test/common/JavaArrayEncoderTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.textway.lapg.test.gen;
+package org.textway.lapg.test.common;
 
 import org.junit.Test;
 import org.textway.lapg.common.JavaArrayEncoder;
