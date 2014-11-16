2827c558dc5e64aedc8d5b0928618d7ff4249a84
Evgeny Gryaznov <egryaznov@gmail.com>
2014-11-16 01:11:13 +0100

    update versions to 0.9.5-SNAPSHOT

--- tm-tool/textmapper/src/org/textmapper/tool/Tool.java
+++ tm-tool/textmapper/src/org/textmapper/tool/Tool.java
@@ -30,7 +30,7 @@ import java.io.*;
  */
 public class Tool {
 
-	public static final String VERSION = "0.9.4/java";
+	public static final String VERSION = "0.9.5/java";
 	public static final String BUILD = "2014";
 
 	public static final String HELP_MESSAGE =
