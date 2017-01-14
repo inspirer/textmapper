4dd30431181c611ca74be0e57e9e79b3d9dd88e9
Evgeny Gryaznov <egryaznov@gmail.com>
2017-01-14 16:11:02 +0100

    Bump version to 0.9.11

--- tm-tool/textmapper/src/org/textmapper/tool/Tool.java
+++ tm-tool/textmapper/src/org/textmapper/tool/Tool.java
@@ -30,7 +30,7 @@ import java.io.*;
  */
 public class Tool {
 
-	public static final String VERSION = "0.9.10/java";
+	public static final String VERSION = "0.9.11/java";
 	public static final String BUILD = "2017";
 
 	public static final String HELP_MESSAGE =
