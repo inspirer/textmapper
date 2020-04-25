aa4c33c6b02243f8f37d99e4367cbc4a2237506e
Evgeny Gryaznov <egryaznov@gmail.com>
2020-04-25 21:36:10 +0200

    Bump version to 0.9.28

--- tm-tool/textmapper/src/org/textmapper/tool/Tool.java
+++ tm-tool/textmapper/src/org/textmapper/tool/Tool.java
@@ -30,7 +30,7 @@ import java.io.*;
  */
 public class Tool {
 
-	public static final String VERSION = "0.9.27/java";
+	public static final String VERSION = "0.9.28/java";
 	public static final String BUILD = "2020";
 
 	public static final String HELP_MESSAGE =
