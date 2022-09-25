b2009efde083d448916880f72134ddbe9cc4cd2b
Evgeny Gryaznov <egryaznov@gmail.com>
2022-09-25 19:28:16 +0200

    Bump version to 0.10.0

--- tm-tool/textmapper/src/org/textmapper/tool/Tool.java
+++ tm-tool/textmapper/src/org/textmapper/tool/Tool.java
@@ -30,8 +30,8 @@ import java.io.*;
  */
 public class Tool {
 
-	public static final String VERSION = "0.9.30/java";
-	public static final String BUILD = "2020";
+	public static final String VERSION = "0.10.0/java";
+	public static final String BUILD = "2022";
 
 	public static final String HELP_MESSAGE =
 			"textmapper - Lexer and Parser generator\n" +
