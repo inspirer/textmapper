30d15a86777d6af7caef6049e13ce0c52d7f35ff
Evgeny Gryaznov <egryaznov@gmail.com>
2023-05-07 21:34:20 +0200

    Add a table test for the JSON lexer in C++. Introduce a markup package.

--- examples/cpp/json/lexer_test.cc
+++ examples/cpp/json/lexer_test.cc
@@ -1,12 +1,68 @@
 #include "lexer.h"
 
+#include <sstream>
 #include <vector>
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "markup/markup.h"
 
 namespace json {
 namespace {
+
+struct Test {
+  Token tok;
+  std::vector<std::string> cases;
+};
+
+inline std::ostream& operator<<(std::ostream& os, Test t) {
+  return os << "{" << t.tok << ", " << t.cases.size() << " cases}";
+}
+
+const std::vector<Test> tests = {
+    {Token::JSONNUMBER,
+     {
+         "«1» «534»",
+         "«1e9» «1.2» «1e-2»",
+     }},
+    {Token::TRUE,
+     {
+         "«true»",
+         "/* true */ «true» ",
+     }},
+};
+
+class LexerTest : public testing::TestWithParam<Test> {};
+
+TEST_P(LexerTest, Token) {
+  const auto& param = GetParam();
+  for (const auto& input : param.cases) {
+    std::vector<markup::Range> want;
+    std::string text;
+    tie(want, text) = markup::Parse(input);
+
+    Lexer l(text);
+    std::vector<markup::Range> tokens;
+    Token next;
+    while ((next = l.Next()) != Token::EOI) {
+      if (next == param.tok) {
+        tokens.push_back(
+            markup::Range{l.TokenStartLocation(), l.TokenEndLocation()});
+      }
+    }
+
+    EXPECT_THAT(tokens, testing::ElementsAreArray(want))
+        << "lexer produced " << markup::Create(text, tokens) << " instead of "
+        << markup::Create(text, want);
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(Vals, LexerTest, testing::ValuesIn(tests),
+                         [](const ::testing::TestParamInfo<Test>& info) {
+                           std::ostringstream os;
+                           os << info.param.tok;
+                           return os.str();
+                         });
 
 const char* json_example = R"(
 {
