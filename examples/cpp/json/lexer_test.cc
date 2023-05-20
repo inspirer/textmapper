e5917af43939120610edaadc6f885e399aa01a52
Evgeny Gryaznov <egryaznov@gmail.com>
2023-05-20 16:35:41 +0200

    Add proper tests on the C++ lexer and set up clangd.

--- examples/cpp/json/lexer_test.cc
+++ examples/cpp/json/lexer_test.cc
@@ -3,40 +3,75 @@
 #include <sstream>
 #include <vector>
 
+#include "markup/markup.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "markup/markup.h"
 
 namespace json {
 namespace {
 
 struct Test {
+  std::string name;
   Token tok;
   std::vector<std::string> cases;
 };
 
-inline std::ostream& operator<<(std::ostream& os, Test t) {
+inline std::ostream &operator<<(std::ostream &os, Test t) {
   return os << "{" << t.tok << ", " << t.cases.size() << " cases}";
 }
 
 const std::vector<Test> tests = {
-    {Token::JSONNUMBER,
+    {"id",
+     Token::ID,
+     {
+         R"( 0«foo» «barB1»)",
+     }},
+    {"string",
+     Token::JSONSTRING,
+     {
+         R"(«"foo"» «"b\nar"»)",
+     }},
+    {"number",
+     Token::JSONNUMBER,
      {
          "«1» «534»",
          "«1e9» «1.2» «1e-2»",
      }},
-    {Token::TRUE,
+    {"true",
+     Token::TRUE,
      {
          "«true»",
          "/* true */ «true» ",
      }},
+
+    {"false", Token::FALSE, {"  «false» "}},
+    {"null", Token::KW_NULL, {"  «null» "}},
+
+    {"lbrace", Token::LBRACE, {"«{»"}},
+    {"rbrace", Token::RBRACE, {"«}»"}},
+    {"lbrack", Token::LBRACK, {"«[»"}},
+    {"rbrack", Token::RBRACK, {"«]»"}},
+    {"colon", Token::COLON, {"«:»"}},
+    {"comma", Token::COMMA, {"«,»"}},
+
+    {"comment", Token::MULTILINECOMMENT, {"  «/*  asda *** */» bar"}},
+
+    // TODO: handle invalid tokens
+    //
+    // {"invalid",
+    //  Token::INVALID_TOKEN,
+    //  {
+    //      "«1e» ",
+    //      "abc   «/*1e  \n»",
+    //      "abc   «\"  »\n",
+    //  }},
 };
 
 class LexerTest : public testing::TestWithParam<Test> {};
 
 TEST_P(LexerTest, Token) {
-  const auto& param = GetParam();
-  for (const auto& input : param.cases) {
+  const auto &param = GetParam();
+  for (const auto &input : param.cases) {
     std::vector<markup::Range> want;
     std::string text;
     tie(want, text) = markup::Parse(input);
@@ -58,53 +93,9 @@ TEST_P(LexerTest, Token) {
 }
 
 INSTANTIATE_TEST_SUITE_P(Vals, LexerTest, testing::ValuesIn(tests),
-                         [](const ::testing::TestParamInfo<Test>& info) {
-                           std::ostringstream os;
-                           os << info.param.tok;
-                           return os.str();
+                         [](const ::testing::TestParamInfo<Test> &info) {
+                           return info.param.name;
                          });
 
-const char* json_example = R"(
-{
-    "some key": [{
-        "title": "example glossary",
-        "float value": 1e9,
-        "float value 2": -0.9e-5,
-		"Gloss \u1234 \nDiv": {
-             "title": "S",   			"items": {
-                "read": {
-                    "ID": "xml",
-					"SortAs": "price",
-					"type": "Markup Language",
-					"Acronym": {},
-					"UniqueID": "850257207432",
-					"def": {
-                "json": "Lorem ipsum dolor sit amet, ad prima imperdiet sea. Homero reprimique no duo, mundi iriure expetenda ei est. No nec denique efficiantur, pri ad oratio adipisci expetendis.",
-						"links": ["ABC", "Echo", "a", "b", "c"]
-                    },
-					"render as": "markup", "null": null, "true": true, "false": false
-                }
-            }
-        }
-    }]
-}
-)";
-
-TEST(LexerTest, Test) {
-  Lexer l(json_example);
-  std::vector<int> tokens;
-  Token next;
-  while ((next = l.Next()) != Token::EOI) {
-    ASSERT_NE(next, Token::INVALID_TOKEN);
-    tokens.push_back(static_cast<int>(next));
-  }
-  EXPECT_THAT(tokens, testing::ElementsAre(
-                          2, 10, 6, 4, 2, 10, 6, 10, 7, 10, 6, 11, 7, 10, 6, 11,
-                          7, 10, 6, 2, 10, 6, 10, 7, 10, 6, 2, 10, 6, 2, 10, 6,
-                          10, 7, 10, 6, 10, 7, 10, 6, 10, 7, 10, 6, 2, 3, 7, 10,
-                          6, 10, 7, 10, 6, 2, 10, 6, 10, 7, 10, 6, 4, 10, 7, 10,
-                          7, 10, 7, 10, 7, 10, 5, 3, 7, 10, 6, 10, 7, 10, 6, 13,
-                          7, 10, 6, 14, 7, 10, 6, 15, 3, 3, 3, 3, 5, 3));
-}
-}  // namespace
-}  // namespace json
+} // namespace
+} // namespace json
