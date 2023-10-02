05dd65012f994cbacdb9e839109a108d41219e76
Evgeny Gryaznov <egryaznov@gmail.com>
2023-10-02 16:57:49 +0200

    Move examples/cpp -> cpp.

--- cpp/gen.sh
+++ cpp/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-go run ../../cmd/textmapper generate -o json/ -compat json/json.tm
+go run ../cmd/textmapper generate -o json/ -compat json/json.tm
 clang-format -i --style=google json/json_lexer* json/json_parser* markup/markup*
 buildifier json/BUILD markup/BUILD ./BUILD
